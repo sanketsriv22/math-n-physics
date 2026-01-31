8f07907787c12f7e4347e30e6be0ed5f9095d068
Sanket Srivastava <sanketsriv22@gmail.com>
2026-01-31 01:05:42 -0500

    seems to work with trivial equation

--- eulers_method.py
+++ eulers_method.py
@@ -10,7 +10,9 @@
 # imports
 import math
 from dataclasses import dataclass
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
+import copy
+import numpy as np
 
 @dataclass
 class Point:
@@ -18,13 +20,17 @@ class Point:
     yn: float
 
 # need a function, using dy/dx = x^2 - y^2, initial condition y(1) = 0
-dydx = lambda Point : Point.xn**2 - Point.yn**2
+# dydx = lambda Point : Point.xn**2 - Point.yn**2
+# IV = Point(1, 0)
 
-IV = Point(1, 0)
+dydx = lambda Point : Point.xn * 2
+IV = Point(0,0)
+
+domain = (IV.xn, IV.xn+6) # arbitrary
 
 h = 0.1 # this is step size
 
-def solveODE(dydx, currPoint: Point, h: float):
+def solve(dydx, currPoint: Point, h: float) -> None:
 
     # take f(x,y) of current point (slope)
     An = dydx(currPoint) 
@@ -33,7 +39,32 @@ def solveODE(dydx, currPoint: Point, h: float):
     currPoint.xn = currPoint.xn + h
     currPoint.yn = currPoint.yn + (h * An)
 
-    return currPoint
+    # return currPoint
 
-# def main():
-#     plt.figure()
+def main():
+    plt.ion()
+    plt.figure(figsize = (8,6))
+    plt.xlim(0, 10)
+    plt.ylim(0, 10)
+    plt.title("Euler's Method Solver for ODEs")
+    plt.grid(True)
+
+    # plot initial value
+    plt.scatter(IV.xn, IV.yn, marker = 'o', s = 5, color = 'k')
+    plt.draw()
+    plt.pause(1)
+
+    currPoint = IV
+    
+    while currPoint.xn <= domain[1]:
+        prevPoint = currPoint
+        solve(dydx, currPoint, h)
+        plt.scatter(currPoint.xn, currPoint.yn, marker='o', s = 5, color = 'k')
+        plt.draw()
+        plt.pause(0.1)
+
+    plt.ioff()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
